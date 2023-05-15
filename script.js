a8dc9ae5b14096be91fb57d262db20aac924db6c
naayalbertini <naayalbertini@gmail.com>
2023-05-15 15:09:20 -0300

    ReservaMesa

---
+++ script.js
@@ -0,0 +1,34 @@
+function sendWhatsApp() {
+    event.preventDefault();
+    const nomeCliente = document.getElementById("nome_cliente").value;
+    const nomeAluno = document.getElementById("nome_aluno").value;
+    const turmaAluno = document.getElementById("turma_aluno").value;
+    const numMesas = document.getElementById("mesas").value;
+    const numMesaAdicional = document.getElementById("mesaAdicional").value;
+    const nomeCadeira = document.getElementById("cadeira").value;
+
+    const mensagem = `Olá, meu nome é ${nomeCliente}.|| Gostaria de reservar as mesas de número: ${numMesas}...|| Adicionei outra mesa: ${numMesaAdicional}. || Essa(s) mesa(s) é em nome do aluno: ${nomeAluno}... || Turma/Ano: ${turmaAluno}... || Acionei uma cadeira: ${nomeCadeira}...|| Me informe o valor total e o PIX?`;
+  
+    const whatsappLink = `https://api.whatsapp.com/send?phone=SEUNUMERO&text=${encodeURIComponent(mensagem)}`;
+  
+    window.location.href = whatsappLink;
+  }
+  
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (event) => {
+    const nomeCliente = document.querySelector('#nome_cliente');
+    const nomeAluno = document.querySelector('#nome_aluno');
+    const turmaAluno = document.querySelector('#turma_aluno');
+    const mesas = document.querySelector('#mesas');
+    const nomeCadeira = document.querySelector('#cadeira');
+    const numMesaAdcional = document.querySelector ('#mesaAdicional');
+    
+  
+    if (!nomeCliente.value || !nomeAluno.value || !turmaAluno.value || !mesas.value) {
+      event.preventDefault(); // cancela o envio do formulário
+      alert('Por favor, preencha todos os campos obrigatórios.');
+    } else {
+      sendWhatsApp();
+    }
+  });
+  
