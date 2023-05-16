0c032a21b6c8f631481b4dc22589c2c538b41101
naaycordeiro <92410157+naayalbertini@users.noreply.github.com>
2023-05-16 11:19:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,4 +31,22 @@ function sendWhatsApp() {
       sendWhatsApp();
     }
   });
+
+// Quando o usuário rolar 20 pixels a partir do topo da página, mostrar o botão
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// Quando o usuário clicar no botão, rolar até o topo da página
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
   
